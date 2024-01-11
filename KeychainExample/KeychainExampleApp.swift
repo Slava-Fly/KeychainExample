fb0b137314a76cc3256a7da5221413d4978afa1a
@Slava-Fly <school.94@bk.ru>
2024-01-11 21:47:13 +0800

    Initial Commit

---
+++ KeychainExample/KeychainExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainExampleApp.swift
+//  KeychainExample
+//
+//  Created by User on 11.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct KeychainExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
