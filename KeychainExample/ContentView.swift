06cdbd9686813773c7bf5deb772a145e5adfdd7e
@Slava-Fly <school.94@bk.ru>
2024-01-16 16:18:42 +0800

    Work with Keychain on SwiftUI

--- KeychainExample/ContentView.swift
+++ KeychainExample/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var password = ""
+    @State private var account = ""
+    @State private var status = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(spacing: 20) {
+                TextField("My password is", text: $password)
+                    .textFieldStyle(.roundedBorder)
+                TextField("My account is", text: $account)
+                    .textFieldStyle(.roundedBorder)
+                Button("Saved data") {
+                    do {
+                        status = try KeychainManager.save(
+                            password: password.data(using: .utf8) ?? Data(),
+                            account: account
+                        )
+                    } catch {
+                        print(error)
+                    }
+                }
+                Button("See my password") {
+                    do {
+                        let data = try KeychainManager.getPassword(for: account)
+                        status = String(decoding: data ?? Data(), as: UTF8.self)
+                    } catch {
+                        print(error)
+                    }
+                }
+                Text(status)
+                    .font(.largeTitle)
+                    .foregroundColor(.red)
+            }
+            .padding()
+            .navigationTitle("Save your Password")
+            Spacer()
         }
-        .padding()
     }
 }
 
